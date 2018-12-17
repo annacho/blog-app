316f5b9e1593feebef377af5736282fd84af017d
annacho <annacho13@gmail.com>
2018-12-17 00:36:38 -0800

    Pass route parameters

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import { Route, NavLink } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
   render () {
@@ -22,17 +23,18 @@ class Blog extends Component {
                   textDecoration: 'underline'
                 }}>Home</NavLink></li>
               <li><NavLink to={{
-                pathname: this.props.match.url + '/new-post',
+                pathname: '/new-post',
                 hash: '#submit',
                 search: '?quick-submit=true'
-              }}>New Post</Link></li>
+              }}>New Post</NavLink></li>
             </ul>
           </nav>
         </header>
-        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+      {/* <Route path="/" exact render={() => <h1>Home</h1>} />
       <Route path="/" render={() => <h1>Home 2</h1>} /> */}
       <Route path="/" exact component={Posts} />
-      <Route path="/new-post" exact component={NewPost} />
+      <Route path="/new-post" component={NewPost} />
+      <Route path="/:id" exact component={FullPost} />
     </div>
     );
   }
