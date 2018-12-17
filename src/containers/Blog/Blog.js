7fe52dd6e6d5878ccc916e3269b99bfdaf83bbb5
annacho <annacho13@gmail.com>
2018-12-17 14:54:00 -0800

    Add lazy loading for routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,11 +4,16 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   }
 
   render () {
